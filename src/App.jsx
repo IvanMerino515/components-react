ce5f872be44fc19d9ad04354301091bddd97b0a7
IvanMerino515 <merdix515@gmail.com>
2024-12-09 17:15:35 +0100

    updtd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import  Button from './components/button/button';
+import CalculateCircleArea from "./components/calculateCircleArea/calculateCircleArea";
+import CalculateSquareArea from "./components/calculateSquareArea/calculateSquareArea";
+import CalculateTriangleArea from "./components/calculateTriangleArea/calculateTriangleArea";
+import CelsiusToFahrenheit from "./components/celsiusToFahrenheit/celsiusToFahrenheit";
+import SayHello from "./components/sayHello/sayHello";
+import TotalPrice from "./components/TotalPrice/totalPrice";
+import WriteMessage from "./components/writeMessage/WriteMessage";
+
+const title = <h1>Curso de React</h1>;
+
+console.log(title)
 
 const App = () => {
 	return <>
-	<Button text = 'Click 1'/>
-	<Button text = 'Click 2'/>
-	<Button text = 'Click 3'/>
+		<SayHello text="Iván"/>
+		<CalculateSquareArea  number={6}/>
+		<CalculateTriangleArea  base={4} height={6}/>
+		<CalculateCircleArea radio={4}/>
+		<CelsiusToFahrenheit celsius={14}/>
+		<TotalPrice price={55}/>
+		<WriteMessage name='Ivan' material='plastico' size='3 onzas' note='Color negro'/>
 	</>
+	;
 };
 
-export default App
+export default App;
+
---
+++ src/components/SayHello/SayHello.jsx
@@ -0,0 +1,5 @@
+const SayHello = ({text}) =>{
+    return <h2>Hola {text}</h2>
+}
+
+export default SayHello;
